65771abf0e858ba810a8968dc4f82d727fb754ef
Gabriel Marius SImu <gabriel.simu@solera.com>
2022-11-18 15:16:19 +0100

    second commit

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movies() { Name = "Pe aripile vantului" };
+            // return View(movie);
+
+            // return Content("Hello World");
+
+            // return HttpNotFound();
+            //   return new EmptyResult();
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1"},
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            var viewResult = new ViewResult();
+            return View(viewModel);
+        }
+
+        [Route("movies/released/{year}/{month}")]
+
+        public ActionResult Edit (int id)
+        {
+            return Content("id = " + id);
+        }
+
+        public ActionResult Index (int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if(string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "name";
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate (int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+    }
+}
---
+++ Models/Movies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
